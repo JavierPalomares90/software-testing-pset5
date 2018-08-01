5386b598bd02634e7666132add1622d26bc0a1c9
Javier Palomares <javier.palomares.90@gmail.com>
2018-07-31 21:05:55 -0500

    Added test classes for reachable

--- src/test/java/GraphGeneratorTest.java
+++ src/test/java/GraphGeneratorTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertTrue;
 public class GraphGeneratorTest
 {
     @Test
-    public void t0() throws ClassNotFoundException
+    public void testCFCForC() throws ClassNotFoundException
     {
         GraphGenerator gg = new GraphGenerator();
         CFG cfg = gg.createCFG("pset5.C"); // example invocation of createCFG
@@ -16,11 +16,29 @@ public class GraphGeneratorTest
     }
 
     @Test
-    public void t1() throws ClassNotFoundException
+    public void testCFGForD() throws ClassNotFoundException
     {
         GraphGenerator gg = new GraphGenerator();
         CFG cfg = gg.createCFGWithMethodInvocation("pset5.D"); // example invocation of createCFGWithMethodInovcation
         System.out.println(cfg.toString());
+        assertTrue(true);
+    }
 
+    @Test
+    public void testReachable() throws ClassNotFoundException
+    {
+        GraphGenerator gg = new GraphGenerator();
+        CFG cfg = gg.createCFGWithMethodInvocation("pset5.D"); // example invocation of createCFGWithMethodInovcation
+        boolean reachable = cfg.isReachable("main", "pset5.D", "foo", "pset5.D");
+        assertTrue(reachable);
+    }
+
+    @Test
+    public void testReachable2() throws ClassNotFoundException
+    {
+        GraphGenerator gg = new GraphGenerator();
+        CFG cfg = gg.createCFGWithMethodInvocation("pset5.D"); // example invocation of createCFGWithMethodInovcation
+        boolean reachable = cfg.isReachable("main", "pset5.D", "bar", "pset5.D");
+        assertTrue(reachable);
     }
 }
