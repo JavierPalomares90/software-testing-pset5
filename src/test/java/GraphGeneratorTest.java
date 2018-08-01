5bbb29c9b6ab8dfb89ceff59498cc9566d8b8ea6
Javier Palomares <javier.palomares.90@gmail.com>
2018-07-31 21:00:05 -0500

    Added test class

--- src/test/java/GraphGeneratorTest.java
+++ src/test/java/GraphGeneratorTest.java
@@ -1,10 +1,26 @@
 import org.junit.Test;
+import pset5.CFG;
+import pset5.GraphGenerator;
+
+import static org.junit.Assert.assertTrue;
 
 public class GraphGeneratorTest
 {
     @Test
-    public void t0()
+    public void t0() throws ClassNotFoundException
     {
+        GraphGenerator gg = new GraphGenerator();
+        CFG cfg = gg.createCFG("pset5.C"); // example invocation of createCFG
+        System.out.println(cfg.toString());
+        assertTrue(true);
+    }
+
+    @Test
+    public void t1() throws ClassNotFoundException
+    {
+        GraphGenerator gg = new GraphGenerator();
+        CFG cfg = gg.createCFGWithMethodInvocation("pset5.D"); // example invocation of createCFGWithMethodInovcation
+        System.out.println(cfg.toString());
 
     }
 }
